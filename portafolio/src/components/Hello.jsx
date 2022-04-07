a6452a8301b3bca2445f0288e6036aecc782ccb1
Ronald Rosero <ronaldes93@gmail.com>
2022-04-06 21:53:58 -0500

    first commit

---
+++ portafolio/src/components/Hello.jsx
@@ -0,0 +1,22 @@
+
+
+import React from 'react'
+
+export default function Hello() {
+    return (
+        <div className='bg-white h-screen flex flex-col
+        justify-center items-center'>
+            <h2 className='font-black text-center mb-4 text-1xl sm:text-2xl md:text-4xl lg:text-6xl'>
+                Hola mi nombre es <br /> Ronald Esteban
+            </h2>
+            <h1 className='font-black text-center mb-2 text-3xl sm:text-5xl md:text-7xl lg:text-9xl'>
+                Desarrollador <br /> Full Stack
+            </h1>
+            <button className="g-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                    PORTAFOLIO
+            </button>
+
+        
+        </div>
+    )
+}
---
+++ portafolio/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
+
+
+
+function Navbar() {
+    return (
+        <nav className="flex justify-between items- h-16 bg-yellow-400 text-black relative shadow-sm" role="navigation"> 
+            <Link 
+                to="/" 
+                className="m-2 flex justify-center lg:justify-start">LOGO
+            </Link>
+            <div className="px-4 cursor-pointer md:hidden ">
+                <div className=" text-black-500 text-3xl">
+                    <FaBars/>
+                </div>  
+            </div>
+            <div className="pr-8  md:block hidden">
+                <Link to="/" className="p-4">
+                    INICIO
+                </Link>
+                <Link to="/portafolio" className="p-4">
+                    PORTAFOLIO
+                </Link>
+                <Link to="/abaut" className="p-4">
+                    SOBRE MÍ
+                </Link>
+                <Link to="/cv" className="p-4">
+                    CV
+                </Link>
+                <Link to="/contact" className="p-4">
+                    CONTACTO
+                </Link>
+                </div>
+        </nav>
+    )
+}
+export default Navbar;
