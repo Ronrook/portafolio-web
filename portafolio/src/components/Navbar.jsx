3b292fbed1c509dc2839392a2735ccb0f5a1ab56
Ronald Rosero <ronaldes93@gmail.com>
2022-04-11 10:06:10 -0500

    deploy

--- portafolio/src/components/Navbar.jsx
+++ portafolio/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ function Navbar({ toggleOpen }) {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                         <a
                         className='flex justify-between items-center w-full text-gray-300'
-                        href='/'
+                        href='https://www.linkedin.com/in/ronald-rosero/' target="_blank"
                         >
                         Linkedin <FaLinkedin size={30} />
                         </a>
@@ -64,7 +64,7 @@ function Navbar({ toggleOpen }) {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                         <a
                         className='flex justify-between items-center w-full text-gray-300'
-                        href='https://github.com/Ronrook'
+                        href='https://github.com/Ronrook' target="_blank"
                         >
                         Github <FaGithub size={30} />
                         </a>
