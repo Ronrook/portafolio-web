140083f26dd19668e96f089e2d90c926441fee8e
Ronald Rosero <ronaldes93@gmail.com>
2022-04-10 20:49:24 -0500

    update main

--- portafolio/src/components/CardHome.jsx
+++ portafolio/src/components/CardHome.jsx
@@ -6,7 +6,7 @@ import dev from '../images/developer.png';
 function CardHome({title, text}) {
     return (
         <article className="bg-white w-60 md:w-72 lg:w-80 h-96 bg-cover bg-center  duration-500  shadow-2xl rounded-xl">
-            <img className="mx-auto w-20 mb-12 mt-10  text-center" src={dev} alt="" />
+            <img className="mx-auto w-20 mb-12 mt-10  text-center" src={dev} alt="profile" />
             <h2 className="text-center text-2xl mt-2 font-bold min-h-18 px-12">
                 {title}
             </h2>
